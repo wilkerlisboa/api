44089bf019f65810513363978677f70863a66a9f
Wilker Lisboa <wilkersoares.wl@gmail.com>
2024-07-19 15:11:01 -0300

    fixing lib

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('cors'); // Adicionado para permitir CORS
 
 const users = require('./users');
 const products = require('./products');
@@ -10,12 +10,8 @@ const students = require('./students');
 const app = express();
 const port = 3000;
 
-// Configuração do CORS com opções mais detalhadas
-app.use(cors({
-  origin: 'http://localhost:5173', // Permitir apenas seu frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Permitir os métodos HTTP necessários
-  allowedHeaders: ['Content-Type', 'Authorization'], // Permitir cabeçalhos necessários
-}));
+// Middleware para permitir CORS
+app.use(cors());
 
 // Middleware para parsing de JSON
 app.use(bodyParser.json());
@@ -25,9 +21,6 @@ app.use('/users', users);
 app.use('/products', products);
 app.use('/employees', employees);
 app.use('/students', students);
-
-// Habilitar CORS para todas as rotas para requisições OPTIONS
-app.options('*', cors());
 
 // Iniciar o servidor
 app.listen(port, () => {
