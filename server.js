9303dba6542fcdaa159277a800afc0cb26ff9357
Wilker Lisboa <wilkersoares.wl@gmail.com>
2024-07-28 20:16:41 -0300

    Fixing

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// app.js
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -7,7 +6,7 @@ const users = require('./routes/users');
 const products = require('./routes/products');
 const employees = require('./routes/employees');
 const students = require('./routes/students');
-const sales = require('./routes/sales'); // Adicione esta linha
+const sales = require('./routes/sales'); // Adiciona esta linha
 
 app.use(bodyParser.json());
 
@@ -15,7 +14,7 @@ app.use('/users', users);
 app.use('/products', products);
 app.use('/employees', employees);
 app.use('/students', students);
-app.use('/sales', sales); // Adicione esta linha
+app.use('/sales', sales); // Adiciona esta linha
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
